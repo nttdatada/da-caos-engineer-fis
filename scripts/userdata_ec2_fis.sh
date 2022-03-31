a46a0de83d8f362bc2d12f5cde0388e3fe68583e
Marcos Cianci <marcos.lopescianci@nttdata.com>
2022-03-30 22:51:32 -0300

    Userdata ec2 fis ...

--- scripts/userdata_ec2_fis.sh
+++ scripts/userdata_ec2_fis.sh
@@ -1,8 +1,7 @@
-
 #!/bin/bash
 
 instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 region=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
 zone=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | grep availabilityZone | awk -F\" '{print $4}')
 
-sudo echo "<h1>ID: $instanceId      Region: $region     AvalaibilityZone: $zone</h1>" >> /var/www/domain/html/index.html
+sudo echo -e "<h1> ID: $instanceId      Region: $region     AvalaibilityZone: $zone </h1>" >> /var/www/domain/html/index.html
